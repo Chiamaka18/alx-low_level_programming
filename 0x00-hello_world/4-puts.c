dac80fd20721312f78d53192b5a06d4b7411f25d
Chiamaka18 <emmanuelchiamaka18@gmail.com>
2022-06-30 18:19:15 -0700

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   * print word to output
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+
+
+int main(void)
+
+{
+
+		printf("Programming is like building a multilingual puzzle\n");
+
+			return (0);
+
+}
