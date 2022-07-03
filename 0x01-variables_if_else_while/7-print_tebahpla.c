5b9f8ecbd4411846ef5d4cda8a032785c40e655e
Chiamaka18 <emmanuelchiamaka18@gmail.com>
2022-07-02 20:37:53 -0700

    task 6

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints the lowercase alphabet in reverse, followed by a new line
+ *
+ * Return: 0 if sucess
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch --)
+	{
+	putchar(ch);
+	}
+	putchar(10);
+
+	return (0);
+}
